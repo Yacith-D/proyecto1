ad8279cd02268700734bd14e1ab20cb821d3259d
Yacith-D <martinezyacith@gmail.com>
2024-05-02 11:38:52 -0600

    termine mi  funciones y mi stylo y termine mi proyecto

--- proyecto1/main.js
+++ proyecto1/main.js
@@ -3,18 +3,23 @@ let input1 = document.getElementById("input1")
 let input2 = document.getElementById("input2")
 let input3 = document.getElementById("input3")
 let div1 = document.getElementById("texto1")
-console.log(boton,input1,input2,input3);
+let parrafo2= document.getElementById("texto2")
+let parrafo3 = document.getElementById("texto3")
+let input4 = document.getElementById("input4")
+let parrafo4 = document.getElementById("texto4")
+
 
 boton.addEventListener("click", function () {
-    div1.innerText = input1.value;
-    div1.innerHTML = input2.value;
+   
+  parrafo2.innerHTML = input2.value;
+    div1.innerHTML = input1.value;
+    parrafo2.innerHTML = input2.value;
+    parrafo3.innerHTML = input3.value;
+    parrafo4.innerHTML = input4.value;
 
     let mostra1 = document.createElement("p")
-  div1.appendChild(mostra1)
+
 })
-
-
-
 
 
 
